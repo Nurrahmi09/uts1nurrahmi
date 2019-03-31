63fc08c3de7db12c08a33a617471bfe403c0e63a
Nur Rahmi <49005228+Nurrahmi09@users.noreply.github.com>
2019-03-31 22:10:44 +0800

    Add files via upload

---
+++ uts1rahmi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout<< " nama           : Nur rahmi" <<endl;
+    cout<< " NIM            : 1829141007" <<endl;
+    cout<< " Kelas          : Tekom B" <<endl;
+    cout<< " tahun angkatan : 2018" <<endl;
+    return 0;
+}
